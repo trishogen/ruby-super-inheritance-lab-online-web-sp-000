996d6206542eac1aa0785532e51a581f36be75ce
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 20:25:17 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,8 +2,8 @@ class ChattyStudent < Student
 
   def hello
     super
-    print "How are you doing today? I'm okay, but I'm kind of tired.
-    Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy!
+    puts "How are you doing today? I'm okay, but I'm kind of tired.\
+    Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy!\
     What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 end
